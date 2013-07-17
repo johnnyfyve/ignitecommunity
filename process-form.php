bd9f5fe89e31172f05406b5b5566a15e901c2557
johnnyfyve <johnnyfyve@gmail.com>
2013-07-17 13:50:12 -0400

    updating process form

--- process-form.php
+++ process-form.php
@@ -9,11 +9,14 @@ $requestMessage = $_POST["message"];
 // *** Then assemble email
 $emailMessage = "Dear IgniteCommunity.net Site Admin:\n\nThe following message was sent through the IgniteCommunity.net contact form:\n\nFrom:  ".$requestName."\nEmail: ".$requestEmail."\nMessage:\n\n".wordwrap($requestMessage, 70, "\r\n")."\n\nIgniteCommunity.net";
 
+$headers = 'From: info@ignitecommunity.net' . "\r\n" .
+    'Reply-To: info@ignitecommunity.net' . "\r\n" .
+    'CC:johnnyfyve@gmai.com';
+
 // *** Send email to recipients
-mail('rachelle1130@yahoo.com', 'IngiteCommunity.net site contact request', $emailMessage, 'From:info@ignitecommunity.net'.\r\n.'CC:johnnyfyve@gmail.com');
+mail('rachelle1130@yahoo.com', 'IngiteCommunity.net site contact request', $emailMessage, $headers);
 
 // *** Create repsonse message for form page
-
 //echo('Thank you - your request has been sent.'.$requestName.', '.$requestEmail.', '.$requestMessage);
 echo('Thank you - your request has been sent.');
 
