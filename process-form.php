d5f53ad30d1e2dcb665927087cd8d65a374c024f
johnnyfyve <johnnyfyve@gmail.com>
2013-07-17 13:26:51 -0400

    deleted a bunch of files

--- process-form.php
+++ process-form.php
@@ -1,20 +1,20 @@
-<?
+<?php
 // *** Form processing code
 
 // *** First get variables
-
-
+$requestName = $_POST["name"];
+$requestEmail = $_POST["email"];  
+$requestMessage = $_POST["message"];
 
 // *** Then assemble email
-
-
+$emailMessage = "Dear IgniteCommunity.net Site Admin:\n\nThe following message was sent through the IgniteCommunity.net contact form:\n\nFrom:  ".$requestName."\nEmail:".$requestEmail."\nMessage:\n\n".wordwrap($requestMessage, 70, "\r\n")."\n\nIgniteCommunity.net";
 
 // *** Send email to recipients
-
+mail('johnnyfyve@gmail.com', 'IngiteCommunity.net site contact request', $emailMessage);
 
 // *** Create repsonse message for form page
 
+//echo('Thank you - your request has been sent.'.$requestName.', '.$requestEmail.', '.$requestMessage);
 echo('Thank you - your request has been sent.');
-
 
 ?>
